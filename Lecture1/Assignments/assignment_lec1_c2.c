0fd37eb7e7dcdcabeeeb2f204078548c97f3f533
ellabellegarcia <99901495+ellabellegarcia@users.noreply.github.com>
2022-03-03 01:44:39 +0800

    Lecture 1 Assignment

---
+++ Lecture1/Assignments/assignment_lec1_c2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int minuend, subtrahend, difference;
+
+    minuend = 87;
+    subtrahend = 15;
+    difference = minuend - subtrahend;
+
+    printf("The difference between %d and %d is %d.\n", minuend, subtrahend, difference);
+
+    return 0;
+}
+
---
+++ Lecture1/Assignments/assignment_lec4_c4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)                              // Uncapitalize V
+{                                           // Missing bracket
+    int sum;                                // Uncapitalize INT
+    /* COMPUTE RESULT */                    // Close the comment
+    sum = 25 + 37 - 19;                     // Missing semi-colon
+    /* DISPLAY RESULTS */                   // Comment must end with */
+    printf ("The answer is %i\n", sum);     // Separate with comma
+
+    return 0;
+}
